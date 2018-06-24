afe095c7ddcc3a13023f72c6c67ade4817f01ccf
Fraser McKay <frasermckay97@gmail.com>
2018-06-24 17:40:25 +0100

    first unit test done

--- js_calculator_start_point/tests/unit/calculator_spec.js
+++ js_calculator_start_point/tests/unit/calculator_spec.js
@@ -5,11 +5,20 @@ describe('calculator', function () {
   beforeEach(function () {
     calculator = new Calculator()
   });
-  
+
   //mocha
   // write unit tests here in the form of "it should do something..."
   it('it has a sample test', function(){
     assert.equal(true, true)
   })
+
+  it("should have 15 by doing 3*5", function(){
+    calculator.previousTotal = 3;
+    calculator.multiply(5);
+    assert.strictEqual(calculator.runningTotal, 15);
+  });
+  
+
+
 
 });
